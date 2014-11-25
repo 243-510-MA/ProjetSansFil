79f7af1173d0a61b790f43794eabe6f6939ace0d
TSO2014 <Maisonneuve2014>
2014-11-25 15:07:39 -0500

    Creation du Projet pour le PIC18LF26K22
    Convertion du Projet pour le PIC18LF26K22

---
+++ Pro/S_Doyon_Configuration_PIC18LF26K22/projet_sansfils_PIC18LF26K22.X/build/default/production/console.p1.d
@@ -0,0 +1,8 @@
+ build/default/production/console.d  \
+ build/default/production/console.p1:  \
+ console.c  \
+console.h  \
+config_24j40.h  \
+miwi_config_pro.h  \
+miwi_config.h  \
+system_config.h 
---
+++ Pro/S_Doyon_Configuration_PIC18LF26K22/projet_sansfils_PIC18LF26K22.X/build/default/production/spi.p1.d
@@ -0,0 +1,17 @@
+ build/default/production/spi.d  \
+ build/default/production/spi.p1:  \
+ spi.c  \
+spi.h  \
+timer.h  \
+console.h  \
+button.h  \
+lcd.h  \
+config_24j40.h  \
+miwi_config_pro.h  \
+miwi_config.h  \
+system.h  \
+delay.h  \
+eeprom.h  \
+serial_flash.h  \
+system_config.h  \
+symbol.h 
